3ec12d200043ff28482ed3d12ee78cacfdd45321
Md Rafid Ul Islam <rfd.009@gmail.com>
2017-04-18 10:58:21 +0600

    this is new project file

---
+++ SpringOracle/src/main/java/com/rafid/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.rafid.config;
+
+/**
+ * Created by ASUS on 4/17/2017.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
