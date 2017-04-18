8d9621cc969324da060148233c2da77eeb1a057b
Md Rafid Ul Islam <rfd.009@gmail.com>
2017-04-18 23:39:57 +0600

    Rafid created registration of users.....And kind of got stuck with Date and Blob....So solve it before proceeding further.

--- SpringOracle/src/main/java/com/rafid/config/SecurityConfiguration.java
+++ SpringOracle/src/main/java/com/rafid/config/SecurityConfiguration.java
@@ -6,13 +6,23 @@ package com.rafid.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().csrfTokenRepository(csrfTokenRepository());
         httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    private CsrfTokenRepository csrfTokenRepository()
+    {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
     }
 
 }
--- SpringOracle/src/main/java/com/rafid/repositories/UserRepository.java
+++ SpringOracle/src/main/java/com/rafid/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.rafid.repositories;
 
 import com.rafid.models.MyUser;
+import com.rafid.models.Users;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Created by ASUS on 17-Apr-17.
+ * Created by ASUS on 4/18/2017.
  */
-public interface UserRepository extends CrudRepository<MyUser, Long> {
-
+public interface UserRepository extends CrudRepository<Users, Long> {
 }
