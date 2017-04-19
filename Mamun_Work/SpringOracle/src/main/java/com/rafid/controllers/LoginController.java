f2ea97608a3b8c06c8d76470e436b725c58a141c
Mamun <ab9mamun@gmail.com>
2017-04-19 16:17:39 +0600

    Login is working fine

--- Mamun_Work/SpringOracle/src/main/java/com/rafid/controllers/LoginController.java
+++ Mamun_Work/SpringOracle/src/main/java/com/rafid/controllers/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
+
 /**
  * Created by ab9ma on 4/19/2017.
  */
@@ -18,10 +20,24 @@ public class LoginController {
     public String userLogin() {
         return "login";
     }
-
+/*
     @PostMapping("/login")
     public @ResponseBody Iterable<Users> loginCheck(@RequestParam("userName") String userName, @RequestParam("password") String password) {
         return userRepository.findByUserNameAndPassword(userName,password);
+    }
+*/
+    @PostMapping("/login")
+    public String checkLogin(@RequestParam("userName") String userName, @RequestParam("password") String password, Model model){
+        Iterable<Users> usersIterable = userRepository.findByUserNameAndPassword(userName, password);
+        Iterator<Users> users = usersIterable.iterator();
+
+       System.out.println(users.hasNext());
+
+       if(users.hasNext()){
+           model.addAttribute("name", userName);
+           return "index";
+       }
+        return "login";
     }
 
 
