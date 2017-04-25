9fc999507a2ae10b44aafe0c7167934396271227
Mamun <ab9mamun@gmail.com>
2017-04-25 23:26:18 +0600

    Profile pic is added to the database, next step is getting it from the db and showing it in the homepage

--- Mamun_Work/SpringOracle/src/main/java/com/rafid/controllers/UploadController.java
+++ Mamun_Work/SpringOracle/src/main/java/com/rafid/controllers/UploadController.java
@@ -3,21 +3,28 @@ package com.rafid.controllers;
 /**
  * Created by ab9ma on 4/24/2017.
  */
+import com.rafid.models.Users;
+import com.rafid.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 
 @Controller
 public class UploadController {
+    @Autowired
+    UserRepository userRepository;
 
     //Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "F://temp//";
@@ -29,7 +36,7 @@ public class UploadController {
 */
     //@RequestMapping(value = "/upload", method = RequestMethod.POST)
     @PostMapping("/uploadPropic") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+    public String singleFileUpload(HttpSession session, @RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
         File tempFile = new File(UPLOADED_FOLDER);
@@ -48,6 +55,16 @@ public class UploadController {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
+
+     ///the following lines of codes are added for saving the file into the database-------------
+
+            List<Users> users = userRepository.findByUserName(session.getAttribute("username").toString());
+            if(!users.isEmpty()){
+                users.get(0).setProfilePic(bytes);
+                userRepository.save(users.get(0));
+            }
+
+            ///database saving end
 
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
--- Mamun_Work/SpringOracle/src/main/java/com/rafid/models/Users.java
+++ Mamun_Work/SpringOracle/src/main/java/com/rafid/models/Users.java
@@ -46,8 +46,8 @@ public class Users implements Serializable{
     @Column(name = "GIT_ACCOUNT_ID", columnDefinition = "VARCHAR(100)")
     String gitUserId;
 
-    //@Lob
-    //private byte[] profilePic;
+    @Lob
+    private byte[] profilePic;
 
     public Users(){
 
@@ -85,7 +85,7 @@ public class Users implements Serializable{
         this.city = city;
         this.zipCode = zipCode;
         this.userId = userId;
-        //this.profilePic = profilePic;
+        this.profilePic = profilePic;
     }
 
     public Users(String userName, String firstName, String lastName, Date birthDate, String email, String password, String gender, String profession, String country, String state, String city, String zipCode) {
@@ -216,9 +216,9 @@ public class Users implements Serializable{
         this.rating = rating;
     }
 
-   /* public byte[] getProfilePic() {return profilePic;}
+    public byte[] getProfilePic() {return profilePic;}
 
     public void setProfilePic(byte[] profilePic) {
         this.profilePic = profilePic;
-    }*/
+    }
 }
